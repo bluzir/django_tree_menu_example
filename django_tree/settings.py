d7b6189d4169f959164c136f1a8c1b74409d245d
Vladislav <kouklevv@gmail.com>
2017-04-27 12:05:41 +0300

    Improvements

--- django_tree/settings.py
+++ django_tree/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = ['127.0.0.1', ]
 
 # Application definition
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'django_tree.urls'
--- django_tree/urls.py
+++ django_tree/urls.py
@@ -13,14 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+import debug_toolbar
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
 from django_tree import settings
-from menu.views import HomeView, MobileView, ComputerView
+from menu.views import HomeView, BlogView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^__debug__/', include(debug_toolbar.urls)),
+    url(r'^blog/', BlogView.as_view(), name='blog'),
     url(r'^', HomeView.as_view(), name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
