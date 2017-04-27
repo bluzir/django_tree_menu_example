b44fc2fde0922fb89e27fcfc5c48eb11e93f251a
Vladislav <kouklevv@gmail.com>
2017-04-27 13:47:06 +0300

    Handle exception

--- menu/templatetags/draw_menu.py
+++ menu/templatetags/draw_menu.py
@@ -8,6 +8,9 @@ register = template.Library()
 
 @register.inclusion_tag('template_tags/menu.html', takes_context=True)
 def draw_menu(context, slug):
-    menu = Menu.objects.prefetch_related('items__items__items__items').get(slug=slug)
-    return {'menu': menu, 'context': context}
+    try:
+        menu = Menu.objects.prefetch_related('items__items__items__items').get(slug=slug)
+        return {'menu': menu, 'context': context}
+    except Menu.DoesNotExist:
+        return {'menu': '', 'context': context}
 
